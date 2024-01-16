36ffe44ee7088490c5ca7abf813d403b6af9a15c
Arz Dev <arz.dev@outlook.com>
2024-01-16 06:35:06 -0600

    [DataAccess] Add IRepository,cs

---
+++ SistemaInventario.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq.Expressions;
+
+namespace SistemaInventario.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> Get(int id);
+
+        Task<IEnumerable<T>> GetAll
+        (
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null,
+            bool isTracking = true
+        );
+
+        Task<T> GetFirst
+        (
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null,
+            bool isTracking = true
+        );
+
+        Task Add(T entity);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
