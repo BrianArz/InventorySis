011d7b9ee39244434196fd0e78ce071a918954f4
Arz Dev <arz.dev@outlook.com>
2024-01-16 07:06:17 -0600

    [DataAccess/Repository] Add IUnitWork implementation on UnitWork.cs

---
+++ SistemaInventario.DataAccess/Repository/UnitWork.cs
@@ -0,0 +1,28 @@
+﻿using SistemaInventario.DataAccess.Data;
+using SistemaInventario.DataAccess.Repository.IRepository;
+
+namespace SistemaInventario.DataAccess.Repository
+{
+    public class UnitWork : IUnitWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IWarehouseRepository Warehouse { get; private set; }
+
+        public UnitWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Warehouse = new WarehouseRepository(_db);
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
