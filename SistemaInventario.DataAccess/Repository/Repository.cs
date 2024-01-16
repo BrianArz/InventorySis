d95d3a089b6e3aa274c894b070ef0f2095b1c11e
Arz Dev <arz.dev@outlook.com>
2024-01-16 06:35:30 -0600

    [DataAccess] Add IRepository implementation on Repository.cs

---
+++ SistemaInventario.DataAccess/Repository/Repository.cs
@@ -0,0 +1,92 @@
+﻿using SistemaInventario.DataAccess.Repository.IRepository;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.DataAccess.Data;
+
+namespace SistemaInventario.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> DbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            DbSet = _db.Set<T>();
+        }
+
+        public async Task Add(T entity)
+        {
+            await DbSet.AddAsync(entity);
+        }
+
+        public async Task<T> Get(int id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null, bool isTracking = true)
+        {
+            IQueryable<T> query = DbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var property in includeProperties.Split(new char[] { ','}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(property);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetFirst(Expression<Func<T, bool>> filter = null, string includeProperties = null, bool isTracking = true)
+        {
+            IQueryable<T> query = DbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var property in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(property);
+                }
+            }
+
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public void Remove(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            DbSet.RemoveRange(entities);
+        }
+    }
+}
