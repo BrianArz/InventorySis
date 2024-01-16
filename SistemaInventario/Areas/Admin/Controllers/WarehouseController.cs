974dc92e506d70ff00b21c7e958d1b5f0591d0ee
Arz Dev <arz.dev@outlook.com>
2024-01-16 07:26:28 -0600

    [Hospital/Admin] Add Warehouse controller

---
+++ SistemaInventario/Areas/Admin/Controllers/WarehouseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.DataAccess.Repository.IRepository;
+
+namespace SistemaInventario.Areas.Admin.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        private readonly IUnitWork _unitWork;
+
+        public WarehouseController(IUnitWork unitWork)
+        {
+            _unitWork = unitWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var all = await _unitWork.Warehouse.GetAll();
+            return Json(new { data = all });
+        }
+
+        #endregion
+    }
+}
