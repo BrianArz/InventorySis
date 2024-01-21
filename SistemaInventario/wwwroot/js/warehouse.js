56c6c4dabfc5ad321b9c4762405ca3025fbf2bd0
Arz Dev <arz.dev@outlook.com>
2024-01-21 15:10:45 -0600

    [Warehouse/Index] Add GetAll table + actions

---
+++ SistemaInventario/wwwroot/js/warehouse.js
@@ -0,0 +1,45 @@
+﻿let datatable;
+
+$(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    datatable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Warehouse/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            { "data": "description", "width": "40%" },
+            {
+                "data": "status",
+                "render": function (data) {
+                    if (data == true) {
+                        return "Active";
+                    }
+                    else {
+                        return "Inactive";
+                    }
+                },
+                "width": "20%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="/Admin/Warehouse/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                            <a onclick=Delete("/Admin/Warehouse/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                <i class="bi bi-trash3-fill"></i>
+                            </a>
+                        </div>
+                    `
+                },
+                "width": "20%"
+            } 
+        ]
+    });
+}
